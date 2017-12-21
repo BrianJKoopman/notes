06e0363cc6b7f889973c59cc1a2f894a46cb6fe8
Brian Koopman <bjk98@cornell.edu>
2017-12-21 14:45:41 -0500

    Pass file to md2html for conversion

--- md2html.py
+++ md2html.py
@@ -1,15 +1,22 @@
-from jinja2 import Template
 import os
+import argparse
 
-f = open('./markdown/example.md', 'r')
+from jinja2 import Template
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help="The markdown file to convert to html.")
+args = parser.parse_args()
+
+f = open(args.file, 'r')
 a = f.read()
 f.close()
 file_contents = a.split('\n')
+page_title = os.path.basename(args.file).replace('.md', '')
 
 template = "./templates/strapdown.html"
 f = open(template, 'r')
 t = Template(f.read())
-output = t.render(note_file = file_contents)
+output = t.render(title = page_title, note_file = file_contents)
 
-with open(os.path.join("./html/", "index.html"), "wb") as fout:
+with open(os.path.join("./html/", "%s.html"%page_title), "wb") as fout:
     fout.write(output)
